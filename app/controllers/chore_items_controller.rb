226c3dcadc162a6c291c37612b0fbbb66a147a83
estherv93 <esthervelez93@gmail.com>
2016-07-14 15:38:55 -0400

    Added Destroy method to Kids

--- app/controllers/chore_items_controller.rb
+++ app/controllers/chore_items_controller.rb
@@ -1,15 +1,25 @@
 class ChoreItemsController < ApplicationController
     before_action :set_kid
+    before_action :set_chore_item, except: [:create]
     
     def create
         @chore_item = @kid.chore_items.create(chore_item_params)
         redirect_to @kid
     end
     
+    def destroy
+        @chore_item.destroy
+        redirect_to chore_lists_path
+    end
+    
     private
     
     def set_kid
         @kid = Kid.find(params[:kid_id])
+    end
+    
+    def set_chore_item
+        @chore_item = @kid.chore_items.find(params[:id])
     end
     
     def chore_item_params
